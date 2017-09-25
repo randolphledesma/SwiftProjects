eced9053ade122f1c061ea47872cd4940588339c
Randolph Ledesma <randolphl@codev.com>
2017-09-25 00:38:35 -0600

    Code generator

--- Hello/templates/select.sql
+++ Hello/templates/select.sql
@@ -1,7 +1,7 @@
 $sql = "SELECT *
         FROM table
         WHERE id=:id";
-$sql = $db->bindVars($sql, ':id', $rep_id, 'integer');
+$sql = $db->bindVars($sql, ':id', $id, 'integer');
 $res = $db->Execute($sql);
 if ($res->RecordCount()) {
     $info = $res->fields;
